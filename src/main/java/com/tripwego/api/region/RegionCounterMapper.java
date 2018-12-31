0851652b82eabf657f82213894ac1bfc001ade60
GUIBERT Julien <guibertjulien@gmail.com>
2018-12-31 13:17:32 +0100

    clean

--- src/main/java/com/tripwego/api/region/RegionCounterMapper.java
+++ src/main/java/com/tripwego/api/region/RegionCounterMapper.java
@@ -2,10 +2,11 @@ package com.tripwego.api.region;
 
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.KeyFactory;
-import com.tripwego.api.Constants;
 import com.tripwego.dto.region.RegionCounter;
 
 import java.util.logging.Logger;
+
+import static com.tripwego.api.Constants.*;
 
 
 public class RegionCounterMapper {
@@ -16,9 +17,9 @@ public class RegionCounterMapper {
         LOGGER.info("--> RegionCounterMapper.map - START");
         final RegionCounter regionCounter = new RegionCounter();
         regionCounter.setId(KeyFactory.keyToString(entity.getKey()));
-        regionCounter.setName(String.valueOf(entity.getProperty(Constants.NAME)));
-        regionCounter.setCode(String.valueOf(entity.getProperty(Constants.CODE)));
-        regionCounter.setCounter((Long) entity.getProperty(Constants.COUNTER));
+        regionCounter.setName(String.valueOf(entity.getProperty(NAME)));
+        regionCounter.setCode(String.valueOf(entity.getProperty(CODE)));
+        regionCounter.setCounter((Long) entity.getProperty(COUNTER));
         LOGGER.info("--> RegionCounterMapper.map - END");
         return regionCounter;
     }
--- src/main/java/com/tripwego/api/region/RegionCounterRepository.java
+++ src/main/java/com/tripwego/api/region/RegionCounterRepository.java
@@ -1,12 +1,13 @@
 package com.tripwego.api.region;
 
 import com.google.appengine.api.datastore.*;
-import com.tripwego.api.Constants;
 import com.tripwego.api.trip.TripQueries;
 import com.tripwego.dto.trip.Trip;
 
 import java.util.*;
 import java.util.logging.Logger;
+
+import static com.tripwego.api.Constants.*;
 
 /**
  * Created by JG on 05/06/16.
@@ -26,7 +27,7 @@ public class RegionCounterRepository {
     private void purge() {
         LOGGER.info("--> RegionCounterRepository.purge - START");
         final List<Key> keysToKill = new ArrayList<>();
-        final Query query = new Query(Constants.KIND_REGION_COUNTER);
+        final Query query = new Query(KIND_REGION_COUNTER);
         List<Entity> entities = datastore.prepare(query.setKeysOnly()).asList(FetchOptions.Builder.withDefaults());
         for (Entity entity : entities) {
             keysToKill.add(entity.getKey());
@@ -53,9 +54,9 @@ public class RegionCounterRepository {
         final Map<String, Integer> treeMap = new TreeMap<>(map);
         // loop a Map
         for (Map.Entry<String, Integer> entry : treeMap.entrySet()) {
-            final Entity entity = new Entity(Constants.KIND_REGION_COUNTER);
-            entity.setProperty(Constants.CODE, entry.getKey());
-            entity.setProperty(Constants.COUNTER, entry.getValue());
+            final Entity entity = new Entity(KIND_REGION_COUNTER);
+            entity.setProperty(CODE, entry.getKey());
+            entity.setProperty(COUNTER, entry.getValue());
             result.add(entity);
         }
         datastore.put(result);
